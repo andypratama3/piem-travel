2d4f1bc3713caa5a5e494176e2126a394354ee60
andypratama3 <andypratama1211@gmail.com>
2024-06-11 22:21:09 +0800

    first Model And category

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Dashboard\DashboardController;
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+
+
+Route::group(['prefix' => 'dashboard'], function () {
+    Route::get('/', DashboardController::class)->name('dashboard.index');
 });
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
