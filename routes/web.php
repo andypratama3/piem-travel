878b3e869942a5c6757686158b4e79d75d4ff73a
andypratama3 <andypratama1211@gmail.com>
2024-06-16 00:40:51 +0800

    add

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,72 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\layouts\Blank;
+
+
+
+
+use App\Http\Controllers\layouts\Fluid;
+use App\Http\Controllers\icons\MdiIcons;
+use App\Http\Controllers\cards\CardBasic;
+use App\Http\Controllers\pages\MiscError;
+use App\Http\Controllers\layouts\Container;
+use App\Http\Controllers\dashboard\Analytics;
+use App\Http\Controllers\layouts\WithoutMenu;
+use App\Http\Controllers\layouts\WithoutNavbar;
+use App\Http\Controllers\user_interface\Alerts;
+use App\Http\Controllers\user_interface\Badges;
+use App\Http\Controllers\user_interface\Footer;
+use App\Http\Controllers\user_interface\Modals;
+use App\Http\Controllers\user_interface\Navbar;
+use App\Http\Controllers\user_interface\Toasts;
+use App\Http\Controllers\user_interface\Buttons;
+use App\Http\Controllers\extended_ui\TextDivider;
+use App\Http\Controllers\user_interface\Carousel;
+use App\Http\Controllers\user_interface\Collapse;
+use App\Http\Controllers\user_interface\Progress;
+use App\Http\Controllers\user_interface\Spinners;
+use App\Http\Controllers\form_elements\BasicInput;
+use App\Http\Controllers\user_interface\Accordion;
+use App\Http\Controllers\user_interface\Dropdowns;
+use App\Http\Controllers\user_interface\Offcanvas;
+use App\Http\Controllers\user_interface\TabsPills;
+use App\Http\Controllers\form_elements\InputGroups;
+use App\Http\Controllers\form_layouts\VerticalForm;
+use App\Http\Controllers\user_interface\ListGroups;
+use App\Http\Controllers\user_interface\Typography;
+use App\Http\Controllers\authentications\LoginBasic;
+use App\Http\Controllers\pages\MiscUnderMaintenance;
+use App\Http\Controllers\form_layouts\HorizontalForm;
+use App\Http\Controllers\tables\Basic as TablesBasic;
+use App\Http\Controllers\extended_ui\PerfectScrollbar;
+use App\Http\Controllers\pages\AccountSettingsAccount;
+use App\Http\Controllers\authentications\RegisterBasic;
 use App\Http\Controllers\Dashboard\DashboardController;
+use App\Http\Controllers\user_interface\TooltipsPopovers;
+use App\Http\Controllers\pages\AccountSettingsConnections;
+use App\Http\Controllers\pages\AccountSettingsNotifications;
+use App\Http\Controllers\authentications\ForgotPasswordBasic;
+use App\Http\Controllers\user_interface\PaginationBreadcrumbs;
+
+
+
+//Dashboard
+
+use App\Http\Controllers\Dashboard\CategoryController as DashboardCategoryController;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
 
 
-// Route::group(['prefix' => 'dashboard'], function () {
-//     // Route::get('/', DashboardController::class)->name('dashboard.index');
-// });
+Route::group(['prefix' => 'dashboard'], function () {
+    Route::get('/', DashboardController::class)->name('dashboard');
+
+    Route::prefix('data')->group(function () {
+        Route::resource('kategori', DashboardCategoryController::class, ['names' => 'kategori']);
+    });
+});
 // Route::middleware([
 //     'auth:sanctum',
 //     config('jetstream.auth_session'),
@@ -21,49 +77,6 @@ use App\Http\Controllers\Dashboard\DashboardController;
 //     })->name('dashboard');
 // });
 
-
-use App\Http\Controllers\dashboard\Analytics;
-use App\Http\Controllers\layouts\WithoutMenu;
-use App\Http\Controllers\layouts\WithoutNavbar;
-use App\Http\Controllers\layouts\Fluid;
-use App\Http\Controllers\layouts\Container;
-use App\Http\Controllers\layouts\Blank;
-use App\Http\Controllers\pages\AccountSettingsAccount;
-use App\Http\Controllers\pages\AccountSettingsNotifications;
-use App\Http\Controllers\pages\AccountSettingsConnections;
-use App\Http\Controllers\pages\MiscError;
-use App\Http\Controllers\pages\MiscUnderMaintenance;
-use App\Http\Controllers\authentications\LoginBasic;
-use App\Http\Controllers\authentications\RegisterBasic;
-use App\Http\Controllers\authentications\ForgotPasswordBasic;
-use App\Http\Controllers\cards\CardBasic;
-use App\Http\Controllers\user_interface\Accordion;
-use App\Http\Controllers\user_interface\Alerts;
-use App\Http\Controllers\user_interface\Badges;
-use App\Http\Controllers\user_interface\Buttons;
-use App\Http\Controllers\user_interface\Carousel;
-use App\Http\Controllers\user_interface\Collapse;
-use App\Http\Controllers\user_interface\Dropdowns;
-use App\Http\Controllers\user_interface\Footer;
-use App\Http\Controllers\user_interface\ListGroups;
-use App\Http\Controllers\user_interface\Modals;
-use App\Http\Controllers\user_interface\Navbar;
-use App\Http\Controllers\user_interface\Offcanvas;
-use App\Http\Controllers\user_interface\PaginationBreadcrumbs;
-use App\Http\Controllers\user_interface\Progress;
-use App\Http\Controllers\user_interface\Spinners;
-use App\Http\Controllers\user_interface\TabsPills;
-use App\Http\Controllers\user_interface\Toasts;
-use App\Http\Controllers\user_interface\TooltipsPopovers;
-use App\Http\Controllers\user_interface\Typography;
-use App\Http\Controllers\extended_ui\PerfectScrollbar;
-use App\Http\Controllers\extended_ui\TextDivider;
-use App\Http\Controllers\icons\MdiIcons;
-use App\Http\Controllers\form_elements\BasicInput;
-use App\Http\Controllers\form_elements\InputGroups;
-use App\Http\Controllers\form_layouts\VerticalForm;
-use App\Http\Controllers\form_layouts\HorizontalForm;
-use App\Http\Controllers\tables\Basic as TablesBasic;
 
 // Main Page Route
 Route::get('/', [Analytics::class, 'index'])->name('dashboard-analytics');
